5e47c1ec2a395fa7084f3d838179dfd60caf33d3
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2021-10-07 11:56:54 -0400

    Added all the colour stages

--- src/settings.py
+++ src/settings.py
@@ -5,13 +5,19 @@ x, y = 120, 100
 
 # Max frames per seconds
 def get_maxfps(text=False):
-    return 60 if not text else 2
+    return 30 if not text else 2
 
 # Size of the window opened
 RESOLUTION = (1280, 1000)
 
+# Set the colours depending on the ages
+BGCOLOUR = (55, 55, 69)
+CELLCOLOURS = [(31, 143, 132),(17, 163, 173),(20, 173, 196),(33, 171, 217),(49, 143, 232),(37, 99, 207),(17, 59, 133),(40, 77, 143),(89, 122, 181),(150, 172, 212),(218, 226, 240)]
+CELLSTAGES = [1,3,5,7,10,15,20,25,30,40,50]
+
 # Radius of the hexes
 RADIUS = 5
+RADIUS = 2
 
 # Padding around the top and left edges
 OFFSET = RADIUS * 1.8
