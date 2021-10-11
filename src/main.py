57062e6268237ec8fed50cf0483b920daf667b55
TTG3333 <ngassamsimon@gmail.com>
2021-10-11 14:09:02 -0400

    Added a check to skip the rules

--- src/main.py
+++ src/main.py
@@ -41,16 +41,19 @@ def simStep(stepping=False):
             cy = cell.y
             cs = cell.alive
             ca = cell.age
+            nextstate = cs
 
-            # Compute all the rules
-            freeze = rules.freeze(b1, cx, cy)
+            # Skip computing the rules and the age for a cell if it and all the cells around it are dead
+            if not (cs == False and b1.aliveAround(cx, cy) == 0):
+                # Compute all the rules
+                freeze = rules.freeze(b1, cx, cy)
 
-            # Combine all the rules (for now simple because we don't have many rules)
-            nextstate = freeze
-            if nextstate == True:
-                ca = ca + 1 if ca is not None else 1
-            else:
-                ca = None
+                # Combine all the rules (for now simple because we don't have many rules)
+                nextstate = freeze
+                if nextstate == True:
+                    ca = ca + 1 if ca is not None else 1
+                else:
+                    ca = None
 
             # Write to the b2
             b2.write(cx, cy, nextstate, ca)
