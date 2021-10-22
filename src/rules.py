b4745bcd81dfe567e633e6ffd352e3dea944ac00
TTG3333 <ngassamsimon@gmail.com>
2021-10-22 14:00:42 -0400

    Added eternalfreeze

--- src/rules.py
+++ src/rules.py
@@ -30,3 +30,21 @@ def freeze(b: Board, x: int, y: int) -> bool:
         else:
             # print("Number of alive is not in lists:", n)
             return False
+
+def eternalFreeze(b: Board, x: int, y: int) -> bool:
+    n = b.aliveAround(x, y)
+    # list of all the adjacent values that will lead to an alive cell
+    good = [1, 3, 4, 5, 6]
+    bad = []  # list of the values that would lead to a dead cell
+    if b.alive(x, y):
+        return True
+    if n != None:
+        if n in good:
+            # print("Number of alive, good:", n, x, y)
+            return True
+        elif n in bad:
+            # print("Number of alive, bad:", n)
+            return False
+        else:
+            # print("Number of alive is not in lists:", n)
+            return False
