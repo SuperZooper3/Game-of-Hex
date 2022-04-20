d973cb51638690e12bcc9df8dbdf60e0a4d63049
Bananagoo <91487577+Bananagoo@users.noreply.github.com>
2022-04-20 12:40:42 -0400

    Stats with curves

--- src/stats.py
+++ src/stats.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plotCellCount(l):
     step = []
     for i in range(len(l)):
         step.append(i)
     cells = l
+    x = np.array(step)
+    y = np.array(cells)
     fig = plt.figure(figsize = (5, 5))
-    plt.plot(step, cells, color ='purple', width = 0.4)
+    plt.plot(x, y, color ='purple')
     plt.xlabel("Le step")
     plt.ylabel("Le nombre de cellules")
     plt.title("Le nombre de cellules en fonction du step")
@@ -17,8 +20,10 @@ def plotAverageAge(l):
     for i in range(len(l)):
         step.append(i)
     averages = l
+    x = np.array(step)
+    y = np.array(averages)
     fig = plt.figure(figsize = (5, 5))
-    plt.bar(step, averages, color ='pink', width = 0.4)
+    plt.plot(x, y, color ='pink')
     plt.xlabel("Le step")
     plt.ylabel("L'age moyen des cellules")
     plt.title("L'age des cellules en fonction du step")
